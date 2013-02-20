ebdc9be8cb2f81ceac6e6c73d9945927787b8e10
Spenser Jones <spenser.e.jones@gmail.com>
2013-02-20 01:26:03 -0700

    Updating to most recent stable release

--- src/actions/chat.js
+++ src/actions/chat.js
@@ -1,4 +1,4 @@
-this.Action = function(message) {
-  event({ type: 'Chat', data: message, entityId: me.id, global: false, relativeTo: me.id, position: [0, 0] });
-  event({ message: message.message, entityId: me.id, global: false, relativeTo: me.id, position: [0, 0] });
+this.Action = function (args) {
+  var message = { username: me.name, message: args.message };
+  event({ speech: true, type: "Chat", message: message.message, data: message, entityId: me.id });
 };
